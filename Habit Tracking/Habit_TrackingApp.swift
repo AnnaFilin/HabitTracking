b8ef2213c6a65576c5c689805c52a0800e37a5cd
Anna Filin <68apple@gmail.com>
2024-10-23 16:50:03 +0300

    Initial Commit

---
+++ Habit Tracking/Habit_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackingApp.swift
+//  Habit Tracking
+//
+//  Created by Anna Filin on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
